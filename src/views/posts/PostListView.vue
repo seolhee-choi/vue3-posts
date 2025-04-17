da22af68b27528e241bb7aac6663cf152357f4c0
seolhee-choi <csheeee38@gmail.com>
2025-04-17 17:54:12 +0900

    first commit

---
+++ src/views/posts/PostListView.vue
@@ -0,0 +1,49 @@
+<template>
+	<div>
+		<h2>게시글 목록</h2>
+		<hr class="my-4" />
+		<div class="row g-3">
+			<div v-for="post in posts" :key="post.id" class="col-4">
+				<PostItem
+					:title="post.title"
+					:content="post.content"
+					:createdAt="post.createdAt"
+					@click="goPage(post.id)"
+				/>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script setup>
+import PostItem from '@/components/posts/PostItem.vue';
+import { getPosts } from '@/api/posts';
+import { ref } from 'vue';
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+const posts = ref([]);
+
+const fetchPosts = () => {
+	posts.value = getPosts();
+};
+fetchPosts();
+
+const goPage = id => {
+	// router.push('/posts/' + id);
+	// router.push(`/posts/${id}`);
+	router.push({
+		name: 'PostDetail',
+		params: {
+			id,
+		},
+		//http://localhost:3000/posts/1?searchText=hello#world
+		// query: {
+		// 	searchText: 'hello',
+		// },
+		// hash: '#world',
+	});
+};
+</script>
+
+<style lang="scss" scoped></style>
